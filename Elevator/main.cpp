4b0c76f49ed26014663e4edb7350d8dfe272695a
YoFlux <65396077+YoFlux@users.noreply.github.com>
2020-07-17 13:50:23 +0800

    Update main.cpp

--- Elevator/main.cpp
+++ Elevator/main.cpp
@@ -1,14 +1,6 @@
 #include "mainshow.h"
 #include <QApplication>
 
-/*
- * 作者：莫一流
- * 学号：20182131047
- * 题目：41-随机电梯使用场景模拟
- */
-
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
